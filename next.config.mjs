c4f9deaa71699a343c7cabaf2769dc6b4217d713
Omar Yaser <omary7549@gmail.com>
2025-06-18 17:51:13 +0300

    fix the slide show 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // يُفعّل التصدير الثابت بدل الأمر next export
+  output: 'export',
+
+  // (اختياري) لجعل الروابط تنتهي بشرطة مائلة وتُنشئ ملف index.html لكل مسار:
+  // trailingSlash: true,
+
+  // (اختياري) لتغيير اسم مجلد الإخراج من `out` إلى مثلاً `dist`:
+  // distDir: 'dist',
 };
 
 export default nextConfig;
